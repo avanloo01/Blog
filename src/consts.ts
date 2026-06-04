82da163065814fa10c246362d44a848894edbbef
avanloo01 <arthur@vanloo.biz>
2026-06-04 14:15:01 +0800

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Lemaiyan Labs';
+export const SITE_DESCRIPTION = 'Arthur\'s blog on real estate PE and technology';
